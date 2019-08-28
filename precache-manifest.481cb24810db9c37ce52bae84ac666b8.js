6b89316cd016b69a9d422a5416063bd76bf3c667
Jason Keung <jason.keungg@gmail.com>
2019-08-28 13:32:07 -0700

    Updates

---
+++ precache-manifest.481cb24810db9c37ce52bae84ac666b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d2dbfe62033bbd435a732fbc815ed4",
+    "url": "/chores/index.html"
+  },
+  {
+    "revision": "436f706d18f7b9f0f01d",
+    "url": "/chores/static/css/main.e33549e7.chunk.css"
+  },
+  {
+    "revision": "9c67a713fe7ca046403c",
+    "url": "/chores/static/js/2.f9cb0832.chunk.js"
+  },
+  {
+    "revision": "436f706d18f7b9f0f01d",
+    "url": "/chores/static/js/main.bcf0e57d.chunk.js"
+  },
+  {
+    "revision": "f2ff9e61757aa6e0e1fd",
+    "url": "/chores/static/js/runtime~main.ce166c5a.js"
+  }
+]);
